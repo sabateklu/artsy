1063feb491c205ebbe72479b87eb2e035e0e7b7f
Saba Teklu <saab.teklu@gmail.com>
2021-03-17 11:42:14 -0600

    retrieved example object for one piece of art to identify data to store and render

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ const api = require('./artsy.config.js');
 const port = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => {
-  // let artist = req.query.artist;
-  // const type = api.token.type;
-  // const token = api.token.token;
-  // search term returns an object of object ids
-  // send another request for each object id
-  // save all of those results in a database (figure out which data points to save)
-  // allow a user to flip through all of the images and on click (or hover or in a modal) reveal information on the art
-
-  let search = req.query.searchTerm
+  let search = req.query.searchTerm || 'rodents'
   axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${search}`)
-  // .then((data) => {
-  //   res.send(data.data);
-  // })
-  // .catch((err) => err)
+  .then((results) => {
+    let artPieces = results.data.objectIDs;
+    artPieces.forEach((objectId) => {
+      axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
+      .then((response) => {
+        res.send(response.data);
+      })
+    })
+  })
+  .catch((err) => err)
 })
 
 
