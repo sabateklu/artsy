0544da7db041249f2d0275a3ca92b755d1816694
Saba Teklu <saab.teklu@gmail.com>
2021-03-23 14:34:16 -0600

    app.get /api/search finally returns intended array of art in response to apicall

--- server/index.js
+++ server/index.js
@@ -8,16 +8,20 @@ let connection = db.connection
 //var hashedPassword = passwordHash.generate('password123');
 const port = 3000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-  let search = req.query.searchTerm || 'atrocious';
+// app.use('/', ()=> {
+
+// })
+
+app.get('/api/search', (req, res) => {
+  let search = req.query.searchTerm || 'rodents';
   let artPiecesObjectIds;
   let collection = [];
 
   axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${search}`)
-  .then((results) => {
+  .then(async (results) => {
     artPiecesObjectIds = results.data.objectIDs;
-    artPiecesObjectIds.forEach((objectId) => {
-      axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
+    for (const objectId of artPiecesObjectIds) {
+      await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
       .then((response) => {
         if(response.data.primaryImage.length > 0) {
           let piece = {
@@ -34,50 +38,17 @@ app.get('/', (req, res) => {
             artistBio: response.data.artistDisplayBio.replace(/"/g, "")
           };
           collection.push(piece);
-
-          connection.query(`INSERT INTO art VALUES (${piece.objectid}, "${piece.primaryImage}", "${piece.department}", "${piece.title}", "${piece.culture}", "${piece.objectDate}", "${piece.medium}", "${piece.region}", "${piece.country}", "${piece.artist}", "${piece.artistBio}");`, (err, success) => {
-            if (err) {
-              console.log(err);
-            } else {
-              connection.query(`INSERT INTO search (objectid, term) VALUES (${objectId}, "${search}");`, (error, positive) => {
-                if (error) {
-                  console.log(error);
-                } else {
-                  connection.query(`SELECT objectid FROM search WHERE term='${search}';`, (searchError, objectIds)=> {
-                    if (searchError) {
-                      console.log(searchError);
-                    } else {
-                      let queryString = 'SELECT * FROM art WHERE';
-                      for (let i = 0; i < objectIds.length; i++) {
-                        let item = objectIds[i];
-                        queryString+= ` objectid=${item.objectid} OR` ;
-                      }
-                      const finalString = queryString.substring(0, queryString.length -4);
-
-                      connection.query(finalString, (finalErr, finalResults) => {
-                        if (finalErr) {
-                          console.log(finalErr);
-                        } else {
-                          console.log('FINAL RESULTS:', finalResults);
-                        }
-                      })
-                    }
-                  })
-                }
-              })
-            }
-          });
-
         }
       })
-    })
-    //connection.end();
+    }
+    res.status(200).send(collection);
   })
   .catch((err) => {
-    console.log(err);
+    res.status(500).send(err);
   })
 })
 
 app.listen(port, ()=> {
   console.log(`listening on ${port}`)
 })
+
