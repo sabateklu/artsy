1737854533da68e74aa7f357fe92863f2197fc8f
Saba Teklu <saab.teklu@gmail.com>
2021-03-26 17:08:43 -0600

    added react hooks and call to search api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import faker from 'faker';
+import About from './About.jsx';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>
-          Hello Saba
-        </h1>
-      </>
-    );
+function App () {
+  const [art, setArt] = useState([]);
+
+  const getArt = (searchTerm = faker.random.word())=> {
+    axios.get(`/api/search?searchTerm=${searchTerm}`)
+    .then((response) => {
+      console.log(response);
+      setArt(response.data);
+    })
+    .catch((err) => {
+      throw err;
+    })
   }
+
+  useEffect(() => {
+    getArt();
+  }, [])
+
+  return (
+    <>
+      <About />
+    </>
+  );
 }
 
 export default App;
