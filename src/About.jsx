9397f82783fd73884b3774635761b748278c38cb
Saba Teklu <saab.teklu@gmail.com>
2021-03-26 17:08:03 -0600

    about component created

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About () {
+  return (
+    <>
+      <h1>
+        Welcome to Artsy!
+      </h1>
+      <p>
+        Artsy is a web app that uses the NY Metropolitan Museum of Art's public API to search their inventory of over 50,000 pieces of art and history and display them to users.
+      </p>
+    </>
+  );
+};
+
+export default About;
+
